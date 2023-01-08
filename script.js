7bf8819c633cab542e29bf044046f673da4daf14
blackray112 <66740315+blackray112@users.noreply.github.com>
2023-01-08 20:10:35 +0100

    deleted ancient JQuery library, small html correcrion added

--- script.js
+++ script.js
@@ -1,25 +1,2 @@
-function tabelka() {
-    $.getJSON('https://my.api.mockaroo.com/kino.json?key=40cbedd0', function(data) {
-      var table = $('<table></table>');
-      $('body').append(table);
-  
-      var headers = Object.keys(data[0]);
-      var headerRow = $('<tr></tr>');
-      headers.forEach(function(header) {
-        var th = $('<th></th>');
-        th.text(header);
-        headerRow.append(th);
-      });
-      table.append(headerRow);
- 
-      data.forEach(function(rowData) {
-        var row = $('<tr></tr>');
-        headers.forEach(function(header) {
-          var td = $('<td></td>');
-          td.text(rowData[header]);
-          row.append(td);
-        });
-        table.append(row);
-      });
-    });
-  }
+"use strict"
+
